eca84a8e10231d04bd71b508772730c0fae1f759
Sanjay <sanjaysj6282@gmail.com>
2022-11-25 03:40:21 +0530

    google auth corrected

--- users/views.py
+++ users/views.py
@@ -6,11 +6,5 @@ from dj_rest_auth.registration.views import SocialLoginView
 
 class GoogleLogin(SocialLoginView): # if you want to use Authorization Code Grant, use this
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://127.0.0.1:8000/account/google"
+    callback_url = "http://127.0.0.1:8000/user/google"
     client_class = OAuth2Client
-
-
-
-
-# class GoogleLogin(SocialLoginView): # if you want to use Implicit Grant, use this
-#     adapter_class = GoogleOAuth2Adapter
