5b932eb171b3b0145d1d6bdada1fe77d882d59d7
Sanjay <sanjaysj6282@gmail.com>
2022-12-08 14:40:57 +0530

    updated admin

--- events/models.py
+++ events/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Workshop(models.Model):
+    name        = models.CharField(max_length=50)
+    date        = models.DateField()
+    venue       = models.CharField(max_length=100)
+    provider    = models.CharField(max_length=50)
+    logo        = models.ImageField(upload_to='workshop/logo', blank=True)
+    details     = models.TextField()
+    link        = models.CharField(max_length=100)
+    price       = models.IntegerField()
+    points      = models.IntegerField()
+    certificate = models.ImageField(upload_to='workshop/certificate', blank=True)
+ 
+    def __str__(self):
+        return self.name
+
+class Lecture(models.Model):
+    name        = models.CharField(max_length=50)
+    date        = models.DateField()
+    venue       = models.CharField(max_length=100)
+    provider    = models.CharField(max_length=50)
+    logo        = models.ImageField(upload_to='lecture/logo', blank=True)
+    details     = models.TextField()
+    link        = models.CharField(max_length=100)
+    price       = models.IntegerField()
+    points      = models.IntegerField()
+    certificate = models.ImageField(upload_to='lecture/certificate', blank=True)
+    speaker     = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+        
+class Exam(models.Model):
+    name        = models.CharField(max_length=50)
+    date        = models.DateField()
+    venue       = models.CharField(max_length=100)
+    provider    = models.CharField(max_length=50)
+    logo        = models.ImageField(upload_to='exam/logo', blank=True)
+    details     = models.TextField()
+    link        = models.CharField(max_length=100)
+    price       = models.IntegerField()
+    points      = models.IntegerField()
+    certificate = models.ImageField(upload_to='exam/certificate', blank=True)
+    eligibility = models.CharField(max_length=50)   
+    
+    def __str__(self):
+        return self.name
--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,24 @@
+
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 
 from .models import userDetails
 
-class userInline(admin.StackedInline):
-    model = userDetails
-    can_delete = False
-    verbose_name_plural = 'userProfile'
+admin.site.register(userDetails)
 
-# Define a new User admin
-class UserAdmin(BaseUserAdmin):
-    inlines = [userInline]
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# from django.contrib.auth.models import User
 
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+# from .models import userDetails
+
+# class userInline(admin.StackedInline):
+#     model = userDetails
+#     can_delete = False
+#     verbose_name_plural = 'userProfile'
+
+# # Define a new User admin
+# class UserAdmin(BaseUserAdmin):
+#     inlines = [userInline]
+
+# admin.site.unregister(User)
+# admin.site.register(User, UserAdmin)
